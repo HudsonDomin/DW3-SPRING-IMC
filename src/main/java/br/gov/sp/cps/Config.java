948c117f32f44c7673284119778c5366b07a25f1
CaioMtho <caiomathol@gmail.com>
2025-04-12 16:46:14 -0300

    0.1.2 - Configurações (arquivo Config)

--- src/main/java/br/gov/sp/cps/Config.java
+++ src/main/java/br/gov/sp/cps/Config.java
@@ -1,4 +1,29 @@
 package br.gov.sp.cps;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Config {
+    @Bean(name = "limiteAbaixoDoPeso")
+    public double limiteAbaixoDoPeso(){
+        return 18,5
+    }
+    @Bean(name = "limitePesoNormal")
+    public double limitePesoNormal(){
+        return 24,9
+    }
+    @Bean(name = "limiteSobrepeso")
+    public double limiteSobrepeso(){
+        return 29,9
+    }
+    @Bean(name = "limiteObesidade1")
+    public double limiteObesidade1(){
+        return 34,9
+    }
+    @Bean(name = "limiteObesidade2")
+    public double limiteObesidade2(){
+        return 39.9
+    }
+
 }
