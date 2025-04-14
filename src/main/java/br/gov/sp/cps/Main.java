2f502a9cbbe5ebaa5e6e8133be90a93fc7499424
CaioMtho <caiomathol@gmail.com>
2025-04-13 23:08:44 -0300

    Mover conteúdo de formulário para index

--- src/main/java/br/gov/sp/cps/Main.java
+++ src/main/java/br/gov/sp/cps/Main.java
@@ -1,7 +1,6 @@
 package br.gov.sp.cps;
 
 import org.springframework.boot.SpringApplication;
-
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
